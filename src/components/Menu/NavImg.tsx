d557ebb443d913143e447be8d8024441ea16327e
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-12-16 10:12:57 +0100

    img rettet

--- src/components/Menu/NavImg.tsx
+++ src/components/Menu/NavImg.tsx
@@ -14,7 +14,7 @@ export const NavImg = () => {
       <Link href="/" aria-label="Abonnement og kasser" className="relative h-full w-1/2 group">
         {/* Gradient overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent pointer-events-none" />
-        <Image className="h-full w-full object-cover" src="/images/WallWine.png" alt="wall with wine" width={227} height={800} />
+        <Image className="h-full w-full object-cover" src="/images/WallWine.webp" alt="wall with wine" width={227} height={800} />
         <figcaption className="absolute inset-x-0 bottom-6 text-center transition-transform duration-200 ease-in-out group-hover:scale-105">
           Abonnement
           <br />& kasser
