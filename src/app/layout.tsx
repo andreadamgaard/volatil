b868c3029912287da3fbdf9bcb0336767bbe154d
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-12-20 00:47:27 +0100

    r

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import "../styles/swiper.css";
 import { Abel } from "next/font/google";
 import Head from "next/head";
 import Loading from "./loading";
@@ -26,12 +27,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const handleStylesheetLoad = (event: React.SyntheticEvent<HTMLLinkElement>) => {
+    const link = event.currentTarget as HTMLLinkElement;
+    link.media = "all";
+  };
   return (
     <html lang="en" className={`${abel.variable}`}>
       <Head>
         <meta name="robots" content="noindex, nofollow" />
         <link rel="preload" href="/fonts/hackney-webfont.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <link rel="preload" href="/images/branches.svg" as="image" />
+        <link rel="preload" href="/styles/globals.css" as="style" />
+        <link rel="stylesheet" href="/styles/globals.css" media="print" onLoad={handleStylesheetLoad} />
+        <link rel="preload" href="/styles/swiper.css" as="style" />
+        <link rel="stylesheet" href="/styles/swiper.css" media="print" onLoad={handleStylesheetLoad} />
       </Head>
       <body className={clsx(abel.className, "antialiased")}>
         <span className=" md:hidden">
--- src/components/boxIndex/AutumnBox.tsx
+++ src/components/boxIndex/AutumnBox.tsx
@@ -10,9 +10,9 @@ export const AutumnBox = ({ data }: { data: BoxesType | null }) => {
 
   return (
     <div className="flex flex-col min-w-44 min-h-72 w-1/2 md:max-w-[60%] md:w-[60%] md:h-[35rem] md:flex-row lg:w-fit">
-      <figure className="relative -z-10 aspect-4/5 border-primary border-2 border-b-0 md:border-b-2 md:border-r-0 transition duration-500 ease-in-out  md:aspect-9/16 md:w-[45%] lg:aspect-4/5 lg:w-[45%] lg:max-w-[30rem] group-hover:scale-105">
-        <Image src={data.image} alt={data.title} fill sizes="(max-width: 768px) 100vw, (max-width: 1024px) 75vw, (max-width: 1440px) 50vw, 33vw" className="object-cover myImg" />
-        {data.discounted === true && <figcaption className="absolute bg-Vblue-100 z-10 text-white font-hackney rounded-xl py-0 px-3 md:px-5 text-base md:text-2xl mt-2 ml-2">On sale</figcaption>}
+      <figure className="relative aspect-[4/5] md:aspect-[9/16] lg:aspect-[4/5] border-primary border-2 border-b-0 md:border-b-2 md:border-r-0 transition duration-500 ease-in-out md:w-[45%] lg:w-[45%] lg:max-w-[30rem] group-hover:scale-105">
+        <Image src={data.image} alt={data.title} layout="fill" objectFit="cover" sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, (max-width: 1440px) 33vw, 25vw" className="myImg" priority />
+        {data.discounted && <figcaption className="absolute bg-Vblue-100 z-10 text-white font-hackney rounded-xl py-1 px-3 md:px-5 text-base md:text-2xl mt-2 ml-2">On sale</figcaption>}
       </figure>
       <BoxText data={data} />
     </div>
