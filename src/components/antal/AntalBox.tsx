d9fbd13a7ad09440ba4383b5b944ed5c16549175
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-01-04 14:44:45 +0100

    bg antal box

--- src/components/antal/AntalBox.tsx
+++ src/components/antal/AntalBox.tsx
@@ -12,7 +12,7 @@ export const AntalBox = ({ size = "small" }: AntalBoxProps) => {
     <NumberField defaultValue={1} minValue={0} maxValue={15} className="flex flex-col gap-1">
       <Label className="font-bold text-sm">Antal:</Label>
       <Group className={clsx("transition ease-in-out duration-200 flex justify-center items-center gap-0.5 px-0.5 rounded", "ring-2 ring-primary", size === "small" && "max-w-12 max-h-8", size === "medium" && "max-w-16 max-h-14 py-0.5")}>
-        <Input className={clsx("font-hackney flex text-center focus:ring-1 focus:ring-hover rounded focus:rounded-sm h-full", size === "small" && " w-7 text-2xl ", size === "medium" && "w-9 h-full text-3xl")} />
+        <Input className={clsx("font-hackney bg-bg flex text-center focus:ring-1 focus:ring-hover rounded focus:rounded-sm h-full", size === "small" && " w-7 text-2xl ", size === "medium" && "w-9 h-full text-3xl")} />
         <span className="flex flex-col gap-0.5">
           <Button className="hover:bg-secondary flex justify-center items-center transition ease-in-out duration-200 rounded w-full h-full" slot="increment">
             <ChevronUp className={clsx(size === "small" && "size-3.5", size === "medium" && "size-5")} />
