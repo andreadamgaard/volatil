410995a015ac8181e06bb2d76e91f08c9e5c5ba7
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-01-04 09:00:06 +0100

    f

--- src/components/vinListe/VinListe.tsx
+++ src/components/vinListe/VinListe.tsx
@@ -71,7 +71,7 @@ export const VinListe = ({ data, sortOption, selectedFilterType, selectedFilterL
         <article key={vin.sku} className="flex flex-col max-w-[30rem] ring-2 ring-primary rounded 2xl:max-w-[30rem]">
           <Link href={vin.handle} intent="wines" className="flex flex-col h-full max-w-[30rem]">
             <figure className="relative w-full overflow-hidden rounded-t group max-w-[30rem] 2xl:max-w-[30rem]">
-              <Image src={vin.image} alt={vin.title} width={400} height={500} className="object-cover object-center w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105" priority={index === 0} />
+              <Image src={vin.image} alt={vin.title} width={400} height={500} className="object-cover object-center w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105" sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, (max-width: 1280px) 25vw, (min-width: 1280px) 25vw" priority={index === 0} />
               <div className="absolute hidden inset-0 px-7 pb-4 md:flex items-end justify-end opacity-0 md:group-hover:opacity-100 md:transition md:duration-300 md:ease-in-out">
                 <CustomButton size="medium" className="px-4 py-2">
                   Køb mig lige?!
