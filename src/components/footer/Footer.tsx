230c1ff92d1c3523e43e13e11d61b216bd44f204
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-01-04 04:34:28 +0100

    items end

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
         <LineOne className="w-52 h-auto stroke-2 md:w-[17rem]" />
       </div>
 
-      <div className="grid gap-6 md:gap-4 md:col-start-2 md:row-start-1 md:max-w-[450px] lg:min-w-96 lg:col-start-3 grid-cols-2 md:grid-cols-1">
+      <div className="grid gap-6 md:gap-4 md:col-start-2 md:row-start-1 md:max-w-[450px] lg:min-w-96 lg:col-start-3 grid-cols-2 md:grid-cols-1 md:place-items-end">
         <div className="flex flex-col gap-1 md:gap-2 w-40 md:w-80">
           <div className="flex flex-col">
             <div className="flex gap-16 justify-between items-center">
