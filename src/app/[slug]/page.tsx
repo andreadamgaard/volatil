1cb4a8dab9a6a7bd16bc18fa8349d797c5b4260d
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-01-04 17:51:16 +0100

    single view

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -35,12 +35,12 @@ export default async function VinPage({ params }) {
 
   return (
     <section className="pt-4 md:pt-6 xl:pt-10">
-      <article className="flex flex-col items-center gap-4 p-5 md:gap-20 md:items-start md:justify-center md:flex-row lg:items-center">
+      <article className="flex flex-col items-center gap-4 md:p-5 md:gap-6 lg:gap-20 md:items-start md:justify-center md:flex-row">
         {/* kun i mobil */}
-        <div className="md:hidden flex flex-col gap-2 w-full items-center">
+        <div className="md:hidden flex flex-col gap-2 w-full items-center pb-4">
           <h1 className="text-[2rem] text-center font-hackney">{vin.navn}</h1>
 
-          <div className="border-t-2 border-primary flex flex-col gap-2.5 py-2 w-full max-w-[20rem] mx-auto">
+          <div className="border-y-2 border-primary flex items-center flex-col gap-2.5 py-2 w-full max-w-[20rem] mx-auto">
             <div className="font-bold flex justify-between items-center w-full">
               <span className="italic text-center">
                 <p>{vin.producent}</p>
@@ -49,28 +49,29 @@ export default async function VinPage({ params }) {
                 <p>{vin.year}</p>
               </span>
             </div>
+            <span className="w-5/6">
+              <hr className="border-primary border-1" />
+            </span>
+            <div className="flex flex-col justify-center items-center">
+              <p className="italic">Smager godt til:</p>
+              <p className="text-xl font-bold">{vin.smager_godt_til}</p>
+            </div>
           </div>
         </div>
 
         {/* Billede */}
-        <figure className="max-w-96 flex justify-center items-center md:w-fit md:max-w-[30rem] relative">
+        <figure className="max-w-96 min-w-40 max-h-[28rem] overflow-hidden flex items-center md:max-h-[35rem] md:w-fit md:max-w-[30rem] rounded-lg relative">
           <Image src={vin.image} alt={vin.navn} priority width={600} height={900} className="object-cover rounded-lg" />
         </figure>
 
         {/* Tekst og data */}
-        <div className="max-w-96 min-w-40 flex flex-col gap-4 md:max-w-[30rem]">
-          {/* kun i mobil */}
-          <div className="flex flex-col md:hidden justify-center items-center">
-            <p className="italic">Smager godt til:</p>
-            <p className="text-xl font-bold">{vin.smager_godt_til}</p>
-          </div>
-
+        <div className="max-w-96 min-w-40 flex flex-col pt-4 gap-4 md:pt-0 md:min-w-96 md:max-w-96 lg:max-w-[30rem] lg:min-w-[30rem]">
           {/* desktop visning */}
           <div className=" hidden md:flex flex-col gap-4">
-            <h1 className="text-5xl text-center font-hackney ">{vin.navn}</h1>
+            <h1 className="text-4xl text-center font-hackney ">{vin.navn}</h1>
 
             <div className="border-y-2 border-primary min-w-40 flex flex-col items-center gap-2.5 py-2">
-              <div className="font-bold flex justify-between text-2xl w-full">
+              <div className="font-bold flex justify-between text-xl w-full">
                 <span className="italic">
                   <p> {vin.producent} </p>
                 </span>
@@ -83,36 +84,37 @@ export default async function VinPage({ params }) {
               </span>
               <div className="flex flex-col justify-center items-center">
                 <p className="italic">Smager godt til:</p>
-                <p className="text-2xl font-bold">{vin.smager_godt_til}</p>
+                <p className="text-xl font-bold">{vin.smager_godt_til}</p>
               </div>
             </div>
           </div>
 
           <BoxSingleView data={vin} />
 
-          <div className="font-hackney text-5xl">
-            <p>{vin.price},00 kr</p>
-          </div>
+          <div className="max-w-96 min-w-40 flex flex-col gap-4 md:min-w-96 md:max-w-96 lg:max-w-[30rem] lg:min-w-[30rem]">
+            <div className="flex flex-col pt-1 ">
+              <p className="font-bold md:text-sm text-base">På lager</p>
+              <StockLine filledWidth="70%" />
+            </div>
+            <div className="font-hackney text-4xl">
+              <p>{vin.price},00 kr</p>
+            </div>
 
-          <div className="flex flex-col pt-1 ">
-            <p className="font-bold md:text-sm text-base">På lager</p>
-            <StockLine filledWidth="70%" />
-          </div>
+            <div className="flex items-end justify-between gap-4 md:gap-10">
+              <AntalBox size="medium" />
+              <CustomButton size="large">KØB MIG!</CustomButton>
+            </div>
 
-          <div className="flex items-end justify-between gap-4 md:gap-10">
-            <AntalBox size="medium" />
-            <CustomButton size="large">KØB MIG!</CustomButton>
-          </div>
-
-          <div>
-            <div className="flex gap-3  xl:gap-4 ">
-              <CheckCheck className="size-5 text-Vblue-100" />
-              <span className="text-xs">
-                <p className=" pb-1">Pickup available at Volatil. Usually ready in 24 hours</p>
-                <Link href="/om-os/" intent="text">
-                  <strong>View store information</strong>
-                </Link>
-              </span>
+            <div>
+              <div className="flex gap-3 xl:gap-4 ">
+                <CheckCheck className="size-5 text-Vblue-100" />
+                <span className="text-xs">
+                  <p className=" pb-1">Pickup available at Volatil. Usually ready in 24 hours</p>
+                  <Link href="/om-os/" intent="text">
+                    <strong>View store information</strong>
+                  </Link>
+                </span>
+              </div>
             </div>
           </div>
         </div>
--- src/components/boxSingleView/BoxSingleView.tsx
+++ src/components/boxSingleView/BoxSingleView.tsx
@@ -8,57 +8,57 @@ type BoxSingleViewProps = {
 
 export const BoxSingleView = ({ data }: BoxSingleViewProps) => {
   return (
-    <div className="bg-bg border-primary border-2 min-w-40 w-full md:max-w-[30rem] justify-center rounded-lg  pt-5 flex flex-col text-base gap-3">
+    <div className="bg-bg border-primary border-2 min-w-80 w-full md:max-w-[30rem] justify-center rounded-lg  pt-5 flex flex-col text-sm lg:text-base gap-3">
       <div className="px-5 pb-6 border-b border-b-primary flex flex-col gap-4">
         <span>
           <p>
-            <strong className="text-sm">Hvad er det for noget?:</strong> <br />
+            <strong className="text-sm md:text-base">Hvad er det for noget?:</strong> <br />
             {data.beskrivelse}
           </p>
         </span>
         <span>
           <p>
-            <strong className="text-sm">Det vigtige spørgsmål, er etiketten flot?!</strong> <br />
+            <strong className="text-sm md:text-base">Det vigtige spørgsmål, er etiketten flot?!</strong> <br />
             {data.flot_etiket || "Ingen ved det?"}
           </p>
         </span>
       </div>
-      <div className="grid  px-4 gap-2 w-full  md:grid-cols-2 md:px-5 md:gap-4 md:py-2 ">
-        <div className="flex flex-col gap-4 md:gap-7 md:border-r md:border-r-primary">
-          <span className="flex gap-4 md:gap-3 items-center">
-            <Grape className="size md:size-6" />
+      <div className="grid w-full grid-cols-2 px-5 gap-4 py-2 ">
+        <div className="flex flex-col justify-between pr-0.5 gap-7 border-r border-r-primary">
+          <span className="flex gap-3 items-start">
+            <Grape className="size-6" />
             <span className="flex flex-wrap">
               <p>
-                <strong className="text-sm">Vindruer:</strong> <br />
+                <strong className="text-sm md:text-base">Vindruer:</strong> <br />
                 {data.druer || "-"}
               </p>
             </span>
           </span>
-          <span className="flex gap-4 md:gap-3 items-center">
-            <Wine className="size md:size-6" />
+          <span className="flex gap-3 items-start">
+            <Wine className="size-6" />
             <span className="flex flex-wrap">
               <p>
-                <strong className="text-sm">Type:</strong> <br />
+                <strong className="text-sm md:text-base">Type:</strong> <br />
                 {data.type || "-"}
               </p>
             </span>
           </span>
         </div>
-        <div className="flex flex-col gap-4 md:gap-7">
-          <span className="flex gap-4 md:gap-3 items-center">
-            <Earth className="size md:size-6" />
+        <div className="flex flex-col gap-7 justify-between">
+          <span className="flex gap-3 items-start">
+            <Earth className="size-6" />
             <span className="flex flex-wrap">
               <p>
-                <strong className="text-sm">Land:</strong> <br />
+                <strong className="text-sm md:text-base">Land:</strong> <br />
                 {data.land || "-"}
               </p>
             </span>
           </span>
-          <span className="flex gap-4 md:gap-3 items-center">
-            <MapPin className="size md:size-6" />
+          <span className="flex gap-3 items-start">
+            <MapPin className="size-6" />
             <span className="flex flex-wrap">
               <p>
-                <strong className="text-sm">Områdeeee:</strong> <br />
+                <strong className="text-sm md:text-base">Områdeeee:</strong> <br />
                 {data.area || "-"}
               </p>
             </span>
@@ -79,7 +79,7 @@ export const BoxSingleView = ({ data }: BoxSingleViewProps) => {
         <div className="py-6 px-4 transition ease-in-out duration-300 flex justify-between items-center md:py-6 md:px-8">
           <span className="border-r">
             <p>
-              <strong className="text-sm">Alkoholprocent:</strong> <br />
+              <strong className="text-sm md:text-base">Alkoholprocent:</strong> <br />
               {data.alkoholprocent || "-"}
             </p>
           </span>
@@ -87,7 +87,7 @@ export const BoxSingleView = ({ data }: BoxSingleViewProps) => {
 
           <span>
             <p>
-              <strong className="text-sm"> Tilsat svolv:</strong>
+              <strong className="text-sm md:text-base"> Tilsat svolv:</strong>
 
               <br />
               {data.tilsat_svovl || "-"}
@@ -96,7 +96,7 @@ export const BoxSingleView = ({ data }: BoxSingleViewProps) => {
           <div className="h-12 w-px bg-primary" />
           <span>
             <p>
-              <strong className="text-sm"> Størrelse:</strong>
+              <strong className="text-sm md:text-base"> Størrelse:</strong>
               <br />
               {data.size || "-"}
             </p>
