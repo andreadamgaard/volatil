8713aeea677b9b65173e050ce46ff7ff276c85cc
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-01-04 16:37:25 +0100

    Alle vine centreret i nav

--- src/components/Menu/Categories/Alle.tsx
+++ src/components/Menu/Categories/Alle.tsx
@@ -4,14 +4,14 @@ import { AllTheWines } from "@/content/svgs/wine/AllTheWines";
 
 export const Alle = () => {
   return (
-    <div className="flex flex-col w-full h-full py-4 px-6 text-base font-bold gap-y-4 items-center">
+    <div className="flex flex-col w-full h-full py-4 px-6 text-base font-bold gap-y-4 justify-center items-center">
       <Link href="/alle-vine/" intent="null" aria-label="Alle vine">
         <span className="flex flex-col gap-y-4">
           <span className="w-fit flex flex-col items-center justify-center">
             <h2 className="font-hackney text-4xl text-center">Se alle vine (OMG!)</h2>
             <LineTwo className="-mt-1" />
           </span>
-          <span className="hover:scale-105 transition ease-in-out duration-200 group w-full">
+          <span className="hover:scale-105 transition ease-in-out duration-200 group w-full flex justify-center items-center">
             <AllTheWines className="w-60" />
           </span>
         </span>
